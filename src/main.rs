d224a305f8878a414af11c1c4240e85aae7612b0
migraf <michael.graf3110@gmail.com>
2021-12-02 08:26:58 +0100

    day 2

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+use std::str::SplitWhitespace;
 
 fn main() {
-    day_1()
+    day_2()
 }
 
 
@@ -13,8 +14,46 @@ fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
     Ok(io::BufReader::new(file).lines())
 }
 
+fn day_2() {
+    let lines = read_lines("input/day_2.txt").unwrap();
+
+    let mut x = 0;
+    let mut y = 0;
+    let mut aim = 0;
+    let mut x2 = 0;
+    let mut y2 = 0;
+    let commands: Vec<String> = lines.map(|line| line.unwrap()).collect();
+    for command in commands {
+        let split: Vec<&str> = command.split_whitespace().collect();
+        match split[0] {
+            "forward" => {
+                let distance: i32 = split[1].parse().unwrap();
+                x += distance;
+                x2 += distance;
+                y2 += aim * distance;
+            },
+            "down" => {
+                let distance: i32 = split[1].parse().unwrap();
+                y -= distance;
+                aim += distance;
+            },
+            "up" => {
+                let distance: i32 = split[1].parse().unwrap();
+                y += distance;
+                aim -= distance;
+            },
+            _ => {
+                println!("Unknown command");
+            }
+        }
+    }
+    println!("Task 1: {}", x.abs() * y.abs());
+    println!("Task 2: {}", x2.abs() * y2.abs());
+
+}
+
 fn day_1() {
-    let lines = read_lines("input/input_1.txt").unwrap();
+    let lines = read_lines("input/day_1.txt").unwrap();
 
     let scans = lines
         .map(|line| line
@@ -33,6 +72,8 @@ fn day_1() {
         previous = scan.clone();
     }
     println!("Task 1: {}", count);
+
+    // Part 2
     previous = scans.windows(3)
         .nth(0)
         .unwrap()
