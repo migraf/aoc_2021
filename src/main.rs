29a9b04dda7265f2709543a3308aa2dd4af213fd
Michael Graf <michael.graf3110@gmail.com>
2021-12-05 09:53:47 +0100

    day3 task 1

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::path::Path;
 use std::str::SplitWhitespace;
 
 fn main() {
-    day_2()
+    day_3()
 }
 
 
@@ -12,6 +12,78 @@ fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
     where P: AsRef<Path>, {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
+}
+
+fn day_3() {
+    let lines: Vec<String> = read_lines("input/day_3.txt")
+        .unwrap()
+        .map(|line| line.unwrap())
+        .collect();
+
+
+    #[derive(Debug)]
+    struct DiagnosticReport {
+        diagnostics: Vec<String>,
+        gamma_rate: i32,
+        epsilon_rate: i32,
+    }
+
+    impl DiagnosticReport {
+        fn power_consumption(&self) -> i32 {
+            let mut one_counts = vec![0; 12];
+            for diagnostic in &self.diagnostics {
+                let numbers: Vec<i32> = diagnostic
+                    .chars()
+                    .map(|c| c.to_digit(10).unwrap() as i32)
+                    .collect();
+                for (index, number) in numbers.iter().enumerate() {
+                    if *number == 1 {
+                        one_counts[index] += 1;
+                    }
+                }
+            }
+            let total = &self.diagnostics.len() / 2;
+
+
+            let gamma_rate_binary = one_counts
+                .iter()
+                .map(|count| if *count > total { 1 } else { 0 })
+                .collect::<Vec<i32>>();
+            let epsilon_rate_binary = gamma_rate_binary
+                .iter()
+                .map(|bit| if *bit == 1 { 0 } else { 1 })
+                .collect::<Vec<i32>>();
+
+
+            let gamma_string = gamma_rate_binary
+                .iter()
+                .map(|bit| bit.to_string())
+                .collect::<Vec<String>>()
+                .join("");
+
+            let epsilon_string = epsilon_rate_binary.
+                iter()
+                .map(|bit| bit.to_string())
+                .collect::<Vec<String>>()
+                .join("");
+
+            let gamma_rate = i32::from_str_radix(&gamma_string, 2).unwrap();
+            let epsilon_rate = i32::from_str_radix(&epsilon_string, 2).unwrap();
+
+            println!("Gamma {:?}, Epsilon {:?}", gamma_rate, epsilon_rate);
+            return gamma_rate * epsilon_rate;
+
+        }
+    }
+
+    let mut diagnostic_report = DiagnosticReport {
+        diagnostics: lines,
+        gamma_rate: 0,
+        epsilon_rate: 0,
+    };
+    let power_consumption = diagnostic_report.power_consumption();
+
+    println!("Power consumption task 1 {:?}", power_consumption);
 }
 
 fn day_2() {
@@ -31,17 +103,17 @@ fn day_2() {
                 x += distance;
                 x2 += distance;
                 y2 += aim * distance;
-            },
+            }
             "down" => {
                 let distance: i32 = split[1].parse().unwrap();
                 y -= distance;
                 aim += distance;
-            },
+            }
             "up" => {
                 let distance: i32 = split[1].parse().unwrap();
                 y += distance;
                 aim -= distance;
-            },
+            }
             _ => {
                 println!("Unknown command");
             }
@@ -49,7 +121,6 @@ fn day_2() {
     }
     println!("Task 1: {}", x.abs() * y.abs());
     println!("Task 2: {}", x2.abs() * y2.abs());
-
 }
 
 fn day_1() {
